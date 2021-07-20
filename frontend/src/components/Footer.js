a0547f1cb351d017cf9fd83b11dbdc37c1e4c633
bamblebam <burhanizrangwala@gmail.com>
2021-07-20 08:01:56 +0530

    added footer component

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,108 @@
+import React from "react";
+import pic from "../static/Images/pic.png";
+
+export default function Footer() {
+  return (
+    <section
+      className="footer"
+      style={{ backgroundColor: "black", color: "white" }}
+    >
+      <footer
+        className="page-footer font-small stylish-color-dark pt-4"
+        style={{ backgroundColor: "black", color: "white" }}
+      >
+        <div className="row" style={{ marginLeft: "1px", marginRight: "15px" }}>
+          <div className="col-md-4 mx-auto text-center">
+            <center>
+              <a className="navbar-brand" href="/index.html">
+                <img
+                  src={pic}
+                  alt="smlra"
+                  style={{ width: "20%", height: "20%" }}
+                />
+              </a>
+            </center>
+            <p>
+              Somaiya Machine Learning Research Association
+              <br />
+              The Artificial Intelligence Community of KJSCE
+            </p>
+          </div>
+          <div className="col-md-4 mx-auto text-center">
+            <br />
+            <p style={{ color: "black", fontSize: "90%" }}>
+              <a href="./contact.html" style={{ textDecoration: "none" }}>
+                <i className="fa fa-map-marker mx-0" /> KJSCE, Mumbai,
+                Maharashtra, India
+              </a>{" "}
+            </p>
+            <br />
+            <p style={{ color: "black", fontSize: "90%" }}>
+              <a
+                href="mailto: smlra-kjsce@somaiya.edu"
+                style={{ textDecoration: "none" }}
+              >
+                <i className="fa fa-envelope" /> <u>smlra-kjsce@somaiya.edu</u>
+              </a>{" "}
+            </p>
+          </div>
+          <div className="col-md-4 mx-auto">
+            <h5 style={{ textAlign: "center" }}>Contact Us</h5>
+            <br />
+            <ul
+              className="list-unstyled list-inline text-center"
+              style={{ color: "#0bebf3" }}
+            >
+              <li className="list-inline-item">
+                <a
+                  className="btn-floating btn-dribbble mx-1"
+                  href="https://github.com/smlra-kjsce"
+                >
+                  <i className="fa fa-github fa-2x"> </i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="btn-floating btn-dribbble mx-1"
+                  href="https://smlra-kjsce.medium.com"
+                >
+                  <i className="fa fa-medium fa-2x"> </i>
+                </a>
+              </li>
+              <li className="list-inline-item" style={{ color: "#0bebf3" }}>
+                <a
+                  className="btn-floating btn-tw mx-1"
+                  href="https://twitter.com/smlra_kjsce"
+                >
+                  <i className="fa fa-twitter fa-2x"> </i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="btn-floating btn-li mx-1"
+                  href="https://www.linkedin.com/company/smlra-kjsce/"
+                >
+                  <i className="fa fa-linkedin fa-2x"> </i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a
+                  className="btn-floating btn-fb mx-1"
+                  href="https://www.facebook.com/smlrakjsce/"
+                >
+                  <i className="fa fa-facebook fa-2x"> </i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-copyright text-center py-3">
+          <p>
+            © 2021 Copyright. Developed and Maintained by{" "}
+            <strong style={{ color: "wheat" }}>SMLRA</strong>
+          </p>
+        </div>
+      </footer>
+    </section>
+  );
+}
