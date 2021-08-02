7410327e7061ea34878d05394162d09a2da16d6c
bamblebam <burhanizrangwala@gmail.com>
2021-08-02 19:07:08 +0530

    added all past events and 21-22 tab commented for future use

--- frontend/src/pages/Initiatives.js
+++ frontend/src/pages/Initiatives.js
@@ -57,7 +57,7 @@ export default function Initiatives() {
             role="tablist"
             style={{ marginBottom: "5%" }}
           >
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <a
                 className="nav-link"
                 id="skills-2021-tab"
@@ -69,7 +69,7 @@ export default function Initiatives() {
               >
                 2021-22
               </a>
-            </li>
+            </li> */}
             <li className="nav-item">
               <a
                 className="nav-link active"
@@ -220,131 +220,48 @@ export default function Initiatives() {
               <br />
               <div className="card-deck">
                 <Initiative
-                  title="Deep Learning in NLP-101"
-                  body="In this student-led symposium, an introduction to
-                  word2vec, GloVe, RNNs, LSTMs, and ULMFiT was given."
-                  photo={NLP101}
-                  date="7th March, 2020"
-                  github="https://github.com/smlra-kjsce/DL-in-NLP-101"
+                  title="Industry Session on “Machine Learning in Cybersecurity”"
+                  body="This industrial session was held by SMLRA for Network
+                  Intelligence (NIIC), Mumbai. In this session, we presented
+                  the applications of Machine Learning in the Cybersecurity
+                  field to help the company tackle their daily challenges in
+                  a more automated fashion."
+                  photo={MLcyber}
+                  date="11th December, 2019"
+                  github="https://github.com/smlra-kjsce/Cyber-ML-DL-101"
                 ></Initiative>
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/ML-Cyber.jpeg"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Industry Session on “Machine Learning in Cybersecurity”
-                    </h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      This industrial session was held by SMLRA for Network
-                      Intelligence (NIIC), Mumbai. In this session, we presented
-                      the applications of Machine Learning in the Cybersecurity
-                      field to help the company tackle their daily challenges in
-                      a more automated fashion.
-                    </p>
-                    <p className="card-text">
-                      11th December, 2019
-                      <small className="text-muted" />
-                    </p>
-                    <a
-                      className=" card-text btn btn-dark text-light"
-                      href="https://github.com/smlra-kjsce/Cyber-ML-DL-101"
-                      role="button"
-                      target="_blank"
-                    >
-                      View on GitHub &nbsp;
-                      <i className="fab fa-github fa-1x"> </i>
-                    </a>
-                  </div>
-                </div>
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/GANs.jpg"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Introduction to Generative Architectures: GANs and VAEs
-                    </h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      In this session, we explored Game Theory, Generative
-                      Models and explored GANs and VAEs. The audience was later
-                      shown a demonstration of using WGANs for generating their
-                      own fake Pokémons!
-                    </p>
-                    <p className="card-text">
-                      3rd-4th October, 2019
-                      <small className="text-muted" />
-                    </p>
-                    <a
-                      className=" card-text btn btn-dark text-light"
-                      href="https://github.com/smlra-kjsce/Introduction-to-VAEs-and-GANs"
-                      role="button"
-                      target="_blank"
-                    >
-                      View on GitHub &nbsp;
-                      <i className="fab fa-github fa-1x"> </i>
-                    </a>
-                  </div>
-                </div>
+                <Initiative
+                  title="Introduction to Generative Architectures: GANs and VAEs"
+                  body="In this session, we explored Game Theory, Generative
+                  Models and explored GANs and VAEs. The audience was later
+                  shown a demonstration of using WGANs for generating their
+                  own fake Pokémons!"
+                  photo={GANs}
+                  date="3rd-4th October, 2019"
+                  github="https://github.com/smlra-kjsce/Introduction-to-VAEs-and-GANs"
+                ></Initiative>
               </div>
               <br />
               <div className="card-deck">
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/mldl101.jpg"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">ML-DL-101</h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      Introduction to Machine Learning and Deep Learning through
-                      algorithms like linear regression, logistic regression,
-                      SVMs, Decision Trees, Neural Networks, CNNs and FGSM
-                      Attack!
-                    </p>
-                    <p className="card-text">
-                      7th September, 2019
-                      <small className="text-muted" />
-                    </p>
-                    <a
-                      className=" card-text btn btn-dark text-light"
-                      href="https://github.com/smlra-kjsce/ML-101"
-                      role="button"
-                      target="_blank"
-                    >
-                      View on GitHub &nbsp;
-                      <i className="fab fa-github fa-1x"> </i>
-                    </a>
-                  </div>
-                </div>
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/resnet.jpg"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title" style={{ textAlign: "justify" }}>
-                      Research Paper Analysis of ResNet: Deep Residual Learning
-                      for Image Recognition
-                    </h5>
-                    <p className="card-text">
-                      In this paper reading session, we explored the ResNet
-                      research paper and saw the marvelous concept of Residual
-                      Learning that revolutionized the Computer Vision sector.
-                    </p>
-                    <p className="card-text">
-                      3rd and 9th August, 2019
-                      <small className="text-muted" />
-                    </p>
-                    {/* <a class=" card-text btn btn-dark text-light href="#" role="button">View on GitHub</a> */}
-                  </div>
-                </div>
+                <Initiative
+                  title="ML-DL-101"
+                  body="Introduction to Machine Learning and Deep Learning through
+                  algorithms like linear regression, logistic regression,
+                  SVMs, Decision Trees, Neural Networks, CNNs and FGSM
+                  Attack!"
+                  photo={mldl101}
+                  date="7th September, 2019"
+                  github="https://github.com/smlra-kjsce/ML-101"
+                ></Initiative>
+                <Initiative
+                  title="Research Paper Analysis of ResNet: Deep Residual Learning
+                  for Image Recognition"
+                  body="In this paper reading session, we explored the ResNet
+                  research paper and saw the marvelous concept of Residual
+                  Learning that revolutionized the Computer Vision sector."
+                  photo={resnet}
+                  date="3rd and 9th August, 2019"
+                ></Initiative>
               </div>
             </div>
             <div
@@ -354,142 +271,61 @@ export default function Initiatives() {
               aria-labelledby="skills-2018-tab"
             >
               <div className="card-deck">
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/rnn.png"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Implementation of Recurrent Neural Networks
-                    </h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      The speaker for this session was{" "}
-                      <a
-                        href="https://www.cse.iitb.ac.in/~kevin.patel/"
-                        target="_blank"
-                      >
-                        Mr. Kevin Patel
-                      </a>
-                      , a Ph.D. scholar from IIT Bombay, working in Center for
-                      Indian Language Technology (CFILT), IIT Bombay, one of the
-                      finest research labs for NLP in India. Mr. Patel discussed
-                      PyTorch, a deep learning framework by Facebook, and why it
-                      is so research-friendly. The concept of Recurrent Neural
-                      Networks (RNNs) was explained in detail with practical
-                      demonstration.{" "}
-                    </p>
-                    <p className="card-text">
-                      7th April, 2019
-                      <small className="text-muted" />
-                    </p>
-                    {/* <a class=" card-text btn btn-dark text-light href="#" role="button">View on GitHub</a> */}
-                  </div>
-                </div>
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/nlp.png"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Speaker session on “Introduction to Natural Language
-                      Processing”
-                    </h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      The speaker for our third spekaer session was{" "}
-                      <a
-                        href="https://www.cse.iitb.ac.in/~kevin.patel/"
-                        target="_blank"
-                      >
-                        Mr. Kevin Patel
-                      </a>
-                      , a Ph.D. scholar from IIT Bombay, working in Center for
-                      Indian Language Technology (CFILT), IIT Bombay, one of the
-                      finest research labs for NLP in India. Mr. Patel covered
-                      various details pertaining to natural language processing
-                      from sentiment analysis to text summarisation and
-                      chatbots. Topics like geographic profiling, SkipGram
-                      model, greedy unsupervised pre-training, and model
-                      explainability were also covered.{" "}
-                    </p>
-                    <p className="card-text">
-                      26th February, 2019
-                      <small className="text-muted" />
-                    </p>
-                    {/* <a class=" card-text btn btn-dark text-light href="#" role="button">View on GitHub</a> */}
-                  </div>
-                </div>
+                <Initiative
+                  title="Implementation of Recurrent Neural Networks"
+                  body="The speaker for this session was Mr. Kevin Patel
+                  , a Ph.D. scholar from IIT Bombay, working in Center for
+                  Indian Language Technology (CFILT), IIT Bombay, one of the
+                  finest research labs for NLP in India. Mr. Patel discussed
+                  PyTorch, a deep learning framework by Facebook, and why it
+                  is so research-friendly. The concept of Recurrent Neural
+                  Networks (RNNs) was explained in detail with practical
+                  demonstration."
+                  photo={rnn}
+                  date="7th April, 2019"
+                ></Initiative>
+                <Initiative
+                  title="Speaker session on “Introduction to Natural Language
+                  Processing”"
+                  body="The speaker for our third spekaer session was Mr. Kevin Patel
+                  , a Ph.D. scholar from IIT Bombay, working in Center for
+                  Indian Language Technology (CFILT), IIT Bombay, one of the
+                  finest research labs for NLP in India. Mr. Patel covered
+                  various details pertaining to natural language processing
+                  from sentiment analysis to text summarisation and
+                  chatbots. Topics like geographic profiling, SkipGram
+                  model, greedy unsupervised pre-training, and model
+                  explainability were also covered."
+                  photo={nlp}
+                  date="26th February, 2019"
+                ></Initiative>
               </div>
               <br />
               <div className="card-deck">
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/rishabh.jpg"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Research paper demonstration on “3D Human Pose Estimation
-                      Using Videos”
-                    </h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      The speaker for the second SMLRA speaker session was{" "}
-                      <a
-                        href="https://www.cse.iitb.ac.in/~rdabral/"
-                        target="_blank"
-                      >
-                        Mr. Rishabh Dabral
-                      </a>
-                      , a Ph.D. scholar from IIT Bombay. Mr. Dabral presented
-                      his research paper that mentions a novel technique for 3D
-                      human pose estimation, which was selected for the European
-                      Conference on Computer Vision (ECCV 2018).{" "}
-                    </p>
-                    <p className="card-text">
-                      20th October, 2018
-                      <small className="text-muted" />
-                    </p>
-                    {/* <a class=" card-text btn btn-dark text-light href="#" role="button">View on GitHub</a> */}
-                  </div>
-                </div>
-                <div className="card">
-                  <img
-                    className="card-img-top"
-                    src="./Images/Events/alexnet.png"
-                    alt="Card image cap"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">
-                      Seminar on “AlexNet: Introduction to CNN”
-                    </h5>
-                    <p className="card-text" style={{ textAlign: "justify" }}>
-                      {" "}
-                      The first SMLRA speaker session. The speaker for this
-                      session was{" "}
-                      <a
-                        href="https://deepai.org/profile/rohan-mahadev"
-                        target="_blank"
-                      >
-                        Mr. Rohan Mahadev
-                      </a>{" "}
-                      from AiToeLabs, Mumbai with its headquarters in Seattle,
-                      USA. The speaker went through the research paper along
-                      with its implementation, explained rudimentary concepts
-                      and helped developed foundation for the understanding of
-                      the AlexNet architecture and its application in Computer
-                      Vision. Detailed analysis of the AlexNet paper with
-                      implementation details was performed.
-                    </p>
-                    <p className="card-text">
-                      22nd September, 2018
-                      <small className="text-muted" />
-                    </p>
-                  </div>
-                </div>
+                <Initiative
+                  title="Research paper demonstration on “3D Human Pose Estimation
+                  Using Videos”"
+                  body="The speaker for the second SMLRA speaker session was Mr. Rishabh Dabral
+                  , a Ph.D. scholar from IIT Bombay. Mr. Dabral presented
+                  his research paper that mentions a novel technique for 3D
+                  human pose estimation, which was selected for the European
+                  Conference on Computer Vision (ECCV 2018)."
+                  photo={rishabh}
+                  date="26th February, 2019"
+                ></Initiative>
+                <Initiative
+                  title="Seminar on “AlexNet: Introduction to CNN”"
+                  body="The first SMLRA speaker session. The speaker for this
+                  session was Mr. Rohan Mahadev from AiToeLabs, Mumbai with its headquarters in Seattle,
+                  USA. The speaker went through the research paper along
+                  with its implementation, explained rudimentary concepts
+                  and helped developed foundation for the understanding of
+                  the AlexNet architecture and its application in Computer
+                  Vision. Detailed analysis of the AlexNet paper with
+                  implementation details was performed."
+                  photo={alexnet}
+                  date="22nd September, 2018"
+                ></Initiative>
               </div>
             </div>
           </div>
